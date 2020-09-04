d83408c468bc85b888f33a2528553359e89819ef
CQU-code-lover <1209895097@qq.com>
2020-09-04 17:09:15 +0800

    新增后台订单查询接口

--- src/web/servlet/data/common/GetTeacherInformation.java
+++ src/web/servlet/data/common/GetTeacherInformation.java
@@ -5,6 +5,7 @@ import tool.BasicTool;
 import tool.FormatCheckTool;
 import tool.PermissionCheck;
 import web.dataBasePacket.Teacher;
+import web.sessionPacket.TeacherSessionPacket;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,8 +16,10 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 //输入字段:  - teacher_name
-//输出字段： - state     1:成功     2：字段错误    3：无法找到教师信息
+//输出字段： - state     1:成功     2：字段错误     3:无权限   4：无法找到教师信息    5:服务器故障
 // - 改变以后的Teacher    - 普通用户   屏蔽密码、balance 当state！=1时 无法获取     - admin   屏蔽密码     - super admin   不屏蔽
+//权限：    目标teacher、superAdmin：获取全部信息       普通用户  state为1的信息（屏蔽balance password）      admin  屏蔽password
+//流程：    1-字段检查     2-教师查询     3-权限查询     4-屏蔽处理     5-返回信息
 @WebServlet("/getTeacherInformation")
 public class GetTeacherInformation extends HttpServlet {
     @Override
@@ -26,14 +29,18 @@ public class GetTeacherInformation extends HttpServlet {
         Teacher teacher = null;
         String teacher_name = req.getParameter("teacher_name");
         if(FormatCheckTool.checkNotNull(teacher_name)){
-             teacher = new Teacher();
+            teacher = new Teacher();
             teacher.teacher_name = teacher_name;
             if(teacher.set()){
                 this.mask(teacher,req.getSession());
+                if(teacher==null){
+                    state =3;     //这种情况是不会存在的 但是必须要写
+                }
                 state = 1;
             }
             else{
-                state = 3;
+                teacher = null;
+                state = 4;
             }
         }
         else{
@@ -42,15 +49,17 @@ public class GetTeacherInformation extends HttpServlet {
         Gson gson = new Gson();
         GetTeacherInformationReturnPacket getTeacherInformationReturnPacket = new GetTeacherInformationReturnPacket();
         getTeacherInformationReturnPacket.state = state;
-        getTeacherInformationReturnPacket.teacher = teacher;
+        getTeacherInformationReturnPacket.data = teacher;
         resp.getWriter().write(gson.toJson(getTeacherInformationReturnPacket));
     }
 
     //根据不同的权限来进行返回数据的处理
+    //如果是没有权限的话 teacher的值为null
+    //权限的检查是由高到低的
     public void mask(Teacher teacher, HttpSession httpSession){
         PermissionCheck permissionCheck = new PermissionCheck(httpSession);
-        if(permissionCheck.checkSuperAdmin()){
-            //super
+        if(permissionCheck.checkSuperAdmin()||teacher.teacher_name.equals(((TeacherSessionPacket)httpSession.getAttribute("Teacher")).teacher_name)){
+            //super、target_teacher
             //do nothing
         }
         else if(permissionCheck.checkAdmin()){
@@ -71,7 +80,7 @@ public class GetTeacherInformation extends HttpServlet {
     }
 
     public class GetTeacherInformationReturnPacket{
-        public Teacher teacher;
+        public Teacher data;
         public int state;
     }
 }
--- src/web/servlet/data/common/GetUserInformation.java
+++ src/web/servlet/data/common/GetUserInformation.java
@@ -1,18 +1,80 @@
 package web.servlet.data.common;
 
+import com.google.gson.Gson;
 import tool.BasicTool;
+import tool.FormatCheckTool;
+import tool.PermissionCheck;
+import web.dataBasePacket.User;
+import web.sessionPacket.TeacherSessionPacket;
+import web.sessionPacket.UserSessionPacket;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+
+//输入：user_name
+//输出： state：    -1 成功     -2 字段错误    -3 无权限      -4 无此user     -5 服务器故障
+//      data
+//权限：    SA/target_user: 所有      A:屏蔽密码       其他:屏蔽密码 balance
+//流程：   1-字段检查    2-用户获取    3-权限检查
 @WebServlet("/getUserInformation")
 public class GetUserInformation extends HttpServlet{
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         BasicTool.setCharacterEncoding(req, resp);
+        int state = 0;
+        User user = null;
+        String user_name = req.getParameter("user_name");
+        if(FormatCheckTool.checkNotNull(user_name)){
+            user = new User();
+            user.user_name=user_name;
+            if(user.set()) {
+                this.mask(user,req.getSession());
+                if(user==null){
+                    state = 3;
+                }
+                else{
+                    state =1;
+                }
+            }
+            else {
+                user=null;
+                state = 4;
+            }
+        }
+        else{
+            state =2;
+        }
+        GetUserInformationReturnPacket g = new GetUserInformationReturnPacket();
+        g.state = state;
+        g.data = user;
+        resp.getWriter().write(new Gson().toJson(g));
+    }
+
+    private void mask(User user, HttpSession httpSession){
+        PermissionCheck permissionCheck = new PermissionCheck(httpSession);
+        if(permissionCheck.checkSuperAdmin()||user.user_name.equals(((UserSessionPacket)httpSession.getAttribute("User")).user_name)){
+            //super、target_teacher
+            //do nothing
+        }
+        else if(permissionCheck.checkAdmin()){
+            //admin
+            user.password = null;
+        }
+        else{
+            //普通
+            user.balance = -1;
+            user.password = null;
+        }
+    }
+
+    public class GetUserInformationReturnPacket{
+        public User data;
+        public int state;
     }
 }
